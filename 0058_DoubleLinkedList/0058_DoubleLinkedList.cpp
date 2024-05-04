4c3762b15a65aa3d891257bd46ba2525415065d4
gheeeeeeee <gheoputra55@gmail.com>
2024-05-04 23:04:01 +0700

    membuat prosedur searchdata serta memasukkan variabel variabel di dalamnya

--- 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
+++ 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
@@ -177,7 +177,26 @@ void revtraverse()
 }
 
 
-
+void searchData()
+{
+	if (listEmpty() == true)
+	{
+		cout << "\nList is empty" << endl;
+	}
+	Node* prev, * curr;
+	prev = curr = NULL;
+	cout << "\nEnter the roll number of the student whose record you want to search: ";
+	int num;
+	cin >> num;
+	if (search(num, &prev, &curr) == false)
+		cout << "\nRecord not found" << endl;
+	else
+	{
+		cout << "\nRecord found" << endl;
+		cout << "\nRoll number: " << curr->noMhs << endl;
+		cout << "\nName: " << curr->name << endl;
+	}
+}
 
 
 

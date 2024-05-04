efa76456ac6642ce70fcb02ca087d47533f67f6f
gheeeeeeee <gheoputra55@gmail.com>
2024-05-04 21:02:47 +0700

    memasukkan algoritma input

--- 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
+++ 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
@@ -12,8 +12,13 @@ struct Node {
 
 Node *START = NULL;
 
-void addNode() {
-
+void addNode() 
+{
+	Node* newNode = new Node(); // step 1: create a new node
+	cout << "\nEnter the roll number of the student: ";
+	cin >> newNode->noMhs;// assign value to the data field of the new node
+	cout << "\nEnter the name of the student: ";
+	cin >> newNode->name; // Assign the value to the data field of the new node
 }
 
 

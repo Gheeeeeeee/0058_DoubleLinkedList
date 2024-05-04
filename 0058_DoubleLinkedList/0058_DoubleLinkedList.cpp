5d282dddba63fed511122ec0e2a4d146621cd162
gheeeeeeee <gheoputra55@gmail.com>
2024-05-04 21:34:06 +0700

    memasukkan list di tengah atau di akhir

--- 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
+++ 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
@@ -38,7 +38,35 @@ void addNode()
 		newNode->prev = NULL; // step 5: make the new node point to NULL
 		START = newNode; // step 6 : make the new node the first node
 	}
+	else
+	{
+	// insert the new node in the middle or at the end
+		Node *current = START; // step 1.a: start from the first node
+		Node* previous = NULL; // step 1.b: previous node is NULL initially
 
+		while(current != NULL && current->noMhs < newNode->noMhs)
+		{							// step 1.c : traverse the list to find the correct position
+			previous = current;		// step 1.d : move the previous to the current node
+			current = current->next;// step 1.e : move the current to the noext node
+		}
+
+		newNode->next = current; // step 1: Make the next field of the new node point to current.
+		newNode->prev = previous; // step 5: Make the previous field of the new node point to previous.
+
+		if (current != NULL)
+		{
+			current->prev = newNode; // step 6: Make the previous field of the current node point to the new node
+		}
+		if (previous != NULL)
+		{
+			previous->next = newNode; // step 7: Make the next fieldof the previous node point to the new node.
+		}
+		else
+		{
+			// if previous is still NULL, its means newNode is now the first node
+			START = newNode;
+		}
+	}
 }
 
 

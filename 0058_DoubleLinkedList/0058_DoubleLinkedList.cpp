7324ecaf9e094580d25c26f16282f4f971ee24dd
gheeeeeeee <gheoputra55@gmail.com>
2024-05-04 22:58:25 +0700

    membuat prosedur retraverse serta memasukkan variabel variabel di dalamnya

--- 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
+++ 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
@@ -157,6 +157,30 @@ void traverse()
 	}
 }
 
+void revtraverse()
+{
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+		cout << "\nRecord in descending order of roll number are:" << endl;
+		Node* currentNode = START;
+		while (currentNode->next != NULL)
+			currentNode = currentNode->next;
+
+		while (currentNode != NULL)
+		{
+			cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->prev;
+		}
+	}
+}
+
+
+
+
+
+
 int main()
 {
 

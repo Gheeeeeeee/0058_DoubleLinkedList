a9c33144441d28cc1ac7a321ae9cab189b7aae70
gheeeeeeee <gheoputra55@gmail.com>
2024-05-04 22:55:34 +0700

    memasukkan variabel variabel conditional statement di dalam prosedur traverse

--- 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
+++ 0058_DoubleLinkedList/0058_DoubleLinkedList.cpp
@@ -143,7 +143,18 @@ bool listEmpty()
 
 void traverse()
 {
-
+	if (listEmpty())
+		cout << "\nList is empty" << endl;
+	else
+	{
+	cout << "\nRecords in ascending order of roll number are:" << endl;
+	Node* currentNode = START;
+	while (currentNode != NULL)
+	{
+		cout << currentNode->noMhs << " " << currentNode->name << endl;
+			currentNode = currentNode->next;
+		}
+	}
 }
 
 int main()
